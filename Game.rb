02ab37da890b357807c1a78cc648e94757f41fa7
fatalaim <fatalsilence1@hotmail.com>
2014-10-15 13:16:14 -0400

    Adds some classes and functions we'll need.

--- Game.rb
+++ Game.rb
@@ -1,15 +1,52 @@
 require 'gosu'
 
+class Block
+	def initialize
+	end
+	
+	def draw
+	end
+	
+	def collide(block)
+	end
+end
+
+class Shape
+	def initialize
+	end
+	
+	def rotation
+	end
+	
+	def draw
+	end
+	
+	def update
+	end
+	
+	def collide
+	end
+end
+
 class GameWindow < Gosu::Window
   def initialize
-    super 640, 480, false
+    super(640, 480, false)
     self.caption = "Tetris"
   end
 
   def update
+	if (button_down?(Gosu::KbEscape))
+		close
+	end
   end
 
   def draw
+  end
+  
+  def next_shape
+  end
+  
+  def line_finished
   end
 end
 
