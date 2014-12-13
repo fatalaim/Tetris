fb232d8e16fd3885cc7ab29f090dcd7ebf222478
John Clevenger <jcleveng@mtu.edu>
2014-12-13 18:52:01 -0500

    Added 2 player mode. Will start on default. Please double check for any bugs.

--- Tetris2.rb
+++ Tetris2.rb
@@ -23,7 +23,6 @@ $gameWidth = 10
 $gameHeight = 16
 
 $delay = 0.5
-$score = 0.0
 
 
 #################################################
@@ -36,10 +35,11 @@ class Block
 
   attr_accessor :xgrid
   attr_accessor :ygrid
-  def initialize(xgridin,ygridin,colorInt)
+  def initialize(xgridin,ygridin,colorInt,player)
 
     @xgrid = xgridin
     @ygrid = ygridin
+    @player = player
 
     @color1 = Color.argb(0xff0000ff)
     @color2 = Color.argb(0xff8888ff)
@@ -68,8 +68,14 @@ class Block
   end
 
   def draw(window)
-    @x = @xgrid*@size
-    @y = @ygrid*@size
+    if @player == 1
+       @x = @xgrid*@size
+       @y = @ygrid*@size
+    end
+    if @player == 2
+       @x = @xgrid*@size - @size*36
+       @y = @ygrid*@size
+    end
     window.draw_quad(@x, 		  @y, 	    @color2,
                      @x+@size,	  @y,	    @color2,
                      @x, 		  @y+@size,   @color2,
@@ -81,8 +87,14 @@ class Block
   end
 
   def drawPreview(window)
-    @x = @xgrid*(@size) - @size*3
-    @y = @ygrid*(@size) + @size*6
+    if @player == 1
+       @x = @xgrid*(@size) - @size*3
+       @y = @ygrid*(@size) + @size*6
+    end
+    if @player == 2
+       @x = @xgrid*(@size) + @size*4
+       @y = @ygrid*(@size) + @size*6
+    end
     window.draw_quad(@x, 		  @y, 	    @color2,
                      @x+@size,	  @y,	    @color2,
                      @x, 		  @y+@size,   @color2,
@@ -108,9 +120,10 @@ end
 class GameGrid
   attr_writer :arrow
 
-  def initialize(window)
+  def initialize(window,player)
     @menuControl =0
     @window = window
+    @player = player
     @font = Gosu::Font.new(@window, Gosu::default_font_name, 20)
     reset()
   end
@@ -135,15 +148,21 @@ class GameGrid
         b.draw @window
       end
 
-      @font.draw("Score: #{Integer($score)}", 15, 10, 0, 1.2, 1.2, 0xff888888)
-      @font.draw("Next: ", 15, 35, 0, 1.2, 1.2, 0xff888888)
+      if @player == 1
+         @font.draw("Score: #{Integer(@score)}", 15, 10, 0, 1.2, 1.2, 0xff888888)
+         @font.draw("Next: ", 15, 35, 0, 1.2, 1.2, 0xff888888)
+      end
+      if @player == 2
+         @font.draw("Score: #{Integer(@score)}", 575, 10, 0, 1.2, 1.2, 0xff888888)
+         @font.draw("Next: ", 575, 35, 0, 1.2, 1.2, 0xff888888)
+      end
     end
   end
 
 #Main loop of the game
   def update()
     #only update after time delay (decreases as more lines cleared)
-    if (Time.now - @oldTime) > ($delay-($score/100))
+    if (Time.now - @oldTime) > ($delay-(@score/100))
       collision = false
 
       #check object below shapes
@@ -183,7 +202,7 @@ class GameGrid
                 b.ygrid += 1
               end
             end
-            $score += 1
+            @score += 1
           end
           count += 1
         end
@@ -213,7 +232,7 @@ class GameGrid
     @shape = Array.new
     @preview = Array.new
     @oldTime = Time.now
-    $score = 0.0
+    @score = 0.0
     newShape()
     previewToShape()
     newShape()
@@ -224,41 +243,82 @@ class GameGrid
     @preview.clear
     @preShapeInt = (1+rand(7))
     @rotation = 0
+    if @player == 1
     if @preShapeInt == 1 #Line
-      @preview.push(Block.new(5,1,1))
-      @preview.push(Block.new(6,1,1))
-      @preview.push(Block.new(7,1,1))
-      @preview.push(Block.new(8,1,1))
+      @preview.push(Block.new(5,1,1,1))
+      @preview.push(Block.new(6,1,1,1))
+      @preview.push(Block.new(7,1,1,1))
+      @preview.push(Block.new(8,1,1,1))
     elsif @preShapeInt == 2 #ReverseL
-      @preview.push(Block.new(5,0,2))
-      @preview.push(Block.new(5,1,2))
-      @preview.push(Block.new(6,1,2))
-      @preview.push(Block.new(7,1,2))
+      @preview.push(Block.new(5,0,2,1))
+      @preview.push(Block.new(5,1,2,1))
+      @preview.push(Block.new(6,1,2,1))
+      @preview.push(Block.new(7,1,2,1))
     elsif @preShapeInt == 3 #LBlock
-      @preview.push(Block.new(8,0,3))
-      @preview.push(Block.new(8,1,3))
-      @preview.push(Block.new(7,1,3))
-      @preview.push(Block.new(6,1,3))
+      @preview.push(Block.new(8,0,3,1))
+      @preview.push(Block.new(8,1,3,1))
+      @preview.push(Block.new(7,1,3,1))
+      @preview.push(Block.new(6,1,3,1))
     elsif @preShapeInt == 4 #Square
-      @preview.push(Block.new(6,1,4))
-      @preview.push(Block.new(6,0,4))
-      @preview.push(Block.new(7,1,4))
-      @preview.push(Block.new(7,0,4))
+      @preview.push(Block.new(6,1,4,1))
+      @preview.push(Block.new(6,0,4,1))
+      @preview.push(Block.new(7,1,4,1))
+      @preview.push(Block.new(7,0,4,1))
     elsif @preShapeInt == 5 #SBlock
-      @preview.push(Block.new(5,1,5))
-      @preview.push(Block.new(6,1,5))
-      @preview.push(Block.new(6,0,5))
-      @preview.push(Block.new(7,0,5))
+      @preview.push(Block.new(5,1,5,1))
+      @preview.push(Block.new(6,1,5,1))
+      @preview.push(Block.new(6,0,5,1))
+      @preview.push(Block.new(7,0,5,1))
     elsif @preShapeInt == 6 #ZBlock
-      @preview.push(Block.new(8,1,6))
-      @preview.push(Block.new(7,1,6))
-      @preview.push(Block.new(7,0,6))
-      @preview.push(Block.new(6,0,6))
+      @preview.push(Block.new(8,1,6,1))
+      @preview.push(Block.new(7,1,6,1))
+      @preview.push(Block.new(7,0,6,1))
+      @preview.push(Block.new(6,0,6,1))
     elsif @preShapeInt == 7 #TBlock
-      @preview.push(Block.new(5,1,7))
-      @preview.push(Block.new(6,1,7))
-      @preview.push(Block.new(6,0,7))
-      @preview.push(Block.new(7,1,7))
+      @preview.push(Block.new(5,1,7,1))
+      @preview.push(Block.new(6,1,7,1))
+      @preview.push(Block.new(6,0,7,1))
+      @preview.push(Block.new(7,1,7,1))
+    end
+    end
+
+    if @player == 2
+    if @preShapeInt == 1 #Line
+      @preview.push(Block.new(55,1,1,2))
+      @preview.push(Block.new(56,1,1,2))
+      @preview.push(Block.new(57,1,1,2))
+      @preview.push(Block.new(58,1,1,2))
+    elsif @preShapeInt == 2 #ReverseL
+      @preview.push(Block.new(55,0,2,2))
+      @preview.push(Block.new(55,1,2,2))
+      @preview.push(Block.new(56,1,2,2))
+      @preview.push(Block.new(57,1,2,2))
+    elsif @preShapeInt == 3 #LBlock
+      @preview.push(Block.new(58,0,3,2))
+      @preview.push(Block.new(58,1,3,2))
+      @preview.push(Block.new(57,1,3,2))
+      @preview.push(Block.new(56,1,3,2))
+    elsif @preShapeInt == 4 #Square
+      @preview.push(Block.new(56,1,4,2))
+      @preview.push(Block.new(56,0,4,2))
+      @preview.push(Block.new(57,1,4,2))
+      @preview.push(Block.new(57,0,4,2))
+    elsif @preShapeInt == 5 #SBlock
+      @preview.push(Block.new(55,1,5,2))
+      @preview.push(Block.new(56,1,5,2))
+      @preview.push(Block.new(56,0,5,2))
+      @preview.push(Block.new(57,0,5,2))
+    elsif @preShapeInt == 6 #ZBlock
+      @preview.push(Block.new(58,1,6,2))
+      @preview.push(Block.new(57,1,6,2))
+      @preview.push(Block.new(57,0,6,2))
+      @preview.push(Block.new(56,0,6,2))
+    elsif @preShapeInt == 7 #TBlock
+      @preview.push(Block.new(55,1,7,2))
+      @preview.push(Block.new(56,1,7,2))
+      @preview.push(Block.new(56,0,7,2))
+      @preview.push(Block.new(57,1,7,2))
+    end
     end
 
     #resize the blocks for preview
@@ -303,7 +363,7 @@ class GameGrid
   def keyRight
     collision = false
     @shape.each do |s|
-      if s.xgrid == 11
+      if s.xgrid == 11 || s.xgrid == 61
         collision = true
         break
       end
@@ -325,7 +385,7 @@ class GameGrid
   def keyLeft
     collision = false
     @shape.each do |s|
-      if s.xgrid == 2
+      if s.xgrid == 2 || s.xgrid == 52
         collision = true
         break
       end
@@ -473,7 +533,7 @@ class GameGrid
     end
 
     #check for rotation collisions
-    if rCollision == false
+    if rCollision == false && @player == 1
       count = 0
       while count < 4
         if rotateArray[count][0] < 2 || rotateArray[count][0] > 11 || rotateArray[count][1] > 18
@@ -490,6 +550,24 @@ class GameGrid
       end
     end
 
+    if rCollision == false && @player == 2
+      count = 0
+      while count < 4
+        if rotateArray[count][0] < 52 || rotateArray[count][0] > 61 || rotateArray[count][1] > 18
+          rCollision = true
+          break
+        end
+        @blocks.each do |b|
+          if rotateArray[count][0] == b.xgrid && rotateArray[count][1] == b.ygrid
+            rCollision = true
+            break
+          end
+        end
+        count += 1
+      end
+    end
+
+
     #apply the rotation if there are no collisions
     if rCollision == false
       count = 0
@@ -504,8 +582,14 @@ class GameGrid
 
 #draw the game background based on global game width, height, and block size
   def drawBackground()
-    x = $blockSize*2
-    y = $blockSize*2
+    if @player == 1
+       x = $blockSize*2 #80
+       y = $blockSize*2 #80
+    end
+    if @player == 2
+       x = ($blockSize*2)+($blockSize*($gameWidth+4)) #640
+       y = $blockSize*2
+    end
     xSize = $blockSize*$gameWidth
     ySize = $blockSize*$gameHeight
 
@@ -533,20 +617,23 @@ end
 class GameWindow < Window
   @menuControl =0
   def initialize
-    super $blockSize*($gameWidth+4), $blockSize*($gameHeight+4), false, 100
-    self.caption = "Tetris 2"
-    @gameGrid = GameGrid.new(self)
+    super $blockSize*(($gameWidth+4)*2), $blockSize*($gameHeight+4), false, 100 #1120x800
+    self.caption = "Tetris"
+    @gameGrid = GameGrid.new(self,1)
+    @gameGrid2 = GameGrid.new(self,2)
   end
 
   #Called automagically by Gosu whenever needed
   def draw()
     @gameGrid.draw()
+    @gameGrid2.draw()
   end
 
   #Called 1/60 times a second, the main logic loop for the game
   def update()
     if @menuControl == 1
       @gameGrid.update
+      @gameGrid2.update
     end
   end
 
@@ -555,6 +642,7 @@ class GameWindow < Window
     if key == KbEscape
       @menuControl =1
       @gameGrid.menuControler(1)
+      @gameGrid2.menuControler(1)
     end
     if key == KbDown
       @gameGrid.keyDown()
@@ -564,6 +652,14 @@ class GameWindow < Window
       @gameGrid.keyLeft()
     elsif key == KbUp
       @gameGrid.keyUp()
+    elsif key == KbW
+      @gameGrid2.keyUp()
+    elsif key == KbA
+      @gameGrid2.keyLeft()
+    elsif key == KbS
+      @gameGrid2.keyDown()
+    elsif key == KbD
+      @gameGrid2.keyRight()
     end
   end
 end
